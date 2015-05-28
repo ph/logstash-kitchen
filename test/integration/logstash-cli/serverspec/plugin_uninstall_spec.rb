30c9624cfb5bace3c51cd9cc7b6220ff92095fe0
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-05-28 12:25:45 -0400

    small uninstall issue

--- test/integration/logstash-cli/serverspec/plugin_uninstall_spec.rb
+++ test/integration/logstash-cli/serverspec/plugin_uninstall_spec.rb
@@ -3,19 +3,16 @@ require_relative "../../../kitchen/files/spec_helper"
 
 describe "bin/plugin uninstall" do
   context "when the plugin isn't installed" do
-    describe command("/opt/logstash/bin/plugin uninstall logstash-output-not-installed") do
-      its(:exit_status) { is_expected.to eq(1) }
+    describe command("/opt/logstash/bin/plugin uninstall logstash-filter-cidr") do
       its(:stdout) { is_expected.to match(/This plugin has not been previously installed, aborting/)  }
+      its(:exit_status) { is_expected.to eq(1) }
     end
   end
 
   context "when the plugin is installed" do
-    describe "plugin uninstall specific plugin" do
-      subject { command("/opt/logstash/bin/plugin uninstall logstash-filter-ruby") }
-
-      its(:exit_status) { is_expected.to eq(0) }
-
+    describe command("/opt/logstash/bin/plugin uninstall logstash-filter-ruby") do
       its(:stdout) { is_expected.to match(/Uninstalling logstash-filter-ruby/) }
+      its(:exit_status) { is_expected.to eq(0) }
     end
   end
 end
