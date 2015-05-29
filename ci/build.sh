8a71fec4fb928ce62cb741afa99018a86a6d6066
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-05-29 10:15:12 -0400

    Make sure use cross platform env for the options

--- ci/build.sh
+++ ci/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "ruby version `ruby -v`"
-bundle exec kitchen test
+# I have to run this through cat, because there is not way to disable colour in test-kitchen.
+# https://github.com/test-kitchen/test-kitchen/issues/330
+bundle exec kitchen test all | cat
