cf1d9765c00cddfdeb66ab40d674ab8dbff905da
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-05-29 10:25:37 -0400

    Disable the colors through cat doesnt work ..

--- ci/build.sh
+++ ci/build.sh
@@ -2,4 +2,4 @@
 echo "ruby version `ruby -v`"
 # I have to run this through cat, because there is not way to disable colour in test-kitchen.
 # https://github.com/test-kitchen/test-kitchen/issues/330
-bundle exec kitchen test all | cat
+bundle exec kitchen test all
