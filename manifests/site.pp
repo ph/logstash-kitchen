9c998424573e18f5b73de0158995fa6bbe8a9cc1
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-05-29 15:24:39 -0400

    Use puppet to install the tar.gz

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,7 @@ class {
   'apt':
     always_apt_update => true;
 }
+
 Package {
   require => Class['apt']
 }
@@ -11,3 +12,17 @@ class { 'elasticsearch':
           repo_version => '1.5',
 }
 elasticsearch::instance { 'logstash-es-1': }
+
+exec { "fetch logstash": 
+  command => "wget -q -O /tmp/logstash.tar.gz ${logstash_package}", 
+  creates => "/tmp/logstash.tar.gz",
+  path => ["/bin", "/usr/bin"]
+}
+file { "/opt/logstash/":
+    ensure => "directory",
+}
+exec { "unpack logstash": 
+  command => "tar -C /opt/logstash -xf /tmp/logstash.tar.gz --strip-components=1", 
+  path => ["/bin", "/usr/bin"],
+  require => [Exec["fetch logstash"], File["/opt/logstash/"]]
+}
