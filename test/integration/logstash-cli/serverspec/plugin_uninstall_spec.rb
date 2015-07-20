a6921f5dd6eb6508b690acf4accae69c95881235
Suyog Rao <suyog.rao@gmail.com>
2015-07-20 15:46:03 -0700

    Update error message check

--- test/integration/logstash-cli/serverspec/plugin_uninstall_spec.rb
+++ test/integration/logstash-cli/serverspec/plugin_uninstall_spec.rb
@@ -4,7 +4,7 @@ require_relative "../../../kitchen/files/spec_helper"
 describe "bin/plugin uninstall" do
   context "when the plugin isn't installed" do
     describe command("/opt/logstash/bin/plugin uninstall logstash-filter-cidr") do
-      its(:stdout) { is_expected.to match(/This plugin has not been previously installed, aborting/)  }
+      its(:stdout) { is_expected.to match(/ERROR: Uninstall Aborted, message: This plugin has not been previously installed, aborting/)  }
       its(:exit_status) { is_expected.to eq(1) }
     end
   end
